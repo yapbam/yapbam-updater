75c601b9e165a1d9483e6b9905a2490475a4c5ba
Astesana <jm-dev@astesana.net>
2026-08-27 19:57:30 +0200

    Add .gitignore, README, improve pom.xml build configuration, try to rename .exe file if not writable and enhance updater error handling

--- src/main/java/net/yapbam/updater/Updater.java
+++ src/main/java/net/yapbam/updater/Updater.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -20,6 +21,8 @@ import net.yapbam.util.Portable;
  */
 public class Updater {
 	private static final String ZIP_FILE = "update.zip"; //$NON-NLS-1$
+	private static final String LOG_FILE = "updater.log"; //$NON-NLS-1$
+	private static final String OLD_SUFFIX = ".old"; //$NON-NLS-1$
 
 	private Updater() {
 		// To prevent instantiation
@@ -31,10 +34,7 @@ public class Updater {
 	public static void main(String[] args) {
 		File installDirectory = Portable.getLaunchDirectory();
 		// Uncompress the zip file
-		try {
-			// Creating input stream
-			ZipInputStream zis = new ZipInputStream(new BufferedInputStream(new FileInputStream(new File(Portable.getUpdateFileDirectory(),ZIP_FILE))));
-
+		try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(new FileInputStream(new File(Portable.getUpdateFileDirectory(),ZIP_FILE))))) {
 			// Read each entry from the ZipInputStream until no more entry found
 			// indicated by a null return value of the getNextEntry() method.
 			for (ZipEntry entry = zis.getNextEntry(); entry != null; entry = zis.getNextEntry()) {
@@ -47,6 +47,18 @@ public class Updater {
 				} else {
 					if (target.isDirectory()) {
 						FileUtils.deleteDirectory(target);
+					}
+					// If the target file is locked (e.g. Yapbam.exe still running on Windows),
+					// try to rename it to <name>.old so the new file can be written.
+					// Windows allows renaming a running executable, but not overwriting it.
+					if (target.exists() && !target.canWrite()) {
+						File old = new File(target.getParentFile(), target.getName()+OLD_SUFFIX);
+						if (old.exists()) {
+							old.delete();
+						}
+						if (!target.renameTo(old)) {
+							throw new IOException("Unable to rename "+target+" to "+old); //$NON-NLS-1$ //$NON-NLS-2$
+						}
 					}
 					FileOutputStream fos = new FileOutputStream(target);
 					byte[] buffer = new byte[10240];
@@ -61,12 +73,31 @@ public class Updater {
 					target.setExecutable(true); //$NON-NLS-1$
 				}
 			}
-			zis.close();
 			JOptionPane.showMessageDialog(null,Messages.getString("Update.Install.success")); //$NON-NLS-1$
 		} catch (IOException e) {
+			logException(e);
 			JOptionPane.showMessageDialog(null,Messages.getString("Update.Install.failure"), //$NON-NLS-1$
 					Messages.getString("Update.Install.title"),JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
 		}
 		FileUtils.deleteDirectory(Portable.getUpdateFileDirectory());
+	}
+
+	/** Writes an exception's stack trace to the updater.log file in the launch directory.
+	 * <br>This file is useful to diagnose update failures that would otherwise be silent
+	 * (the updater must never write to stdout/stderr, see README).
+	 * @param e The exception to log.
+	 */
+	private static void logException(IOException e) {
+		File log = new File(Portable.getLaunchDirectory(), LOG_FILE);
+		try {
+			PrintStream ps = new PrintStream(log, "UTF-8"); //$NON-NLS-1$
+			try {
+				e.printStackTrace(ps);
+			} finally {
+				ps.close();
+			}
+		} catch (IOException logError) {
+			// If we can't even write the log, there's nothing more we can do silently.
+		}
 	}
 }
